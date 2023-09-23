668c26c30edd77bbd67dd22a414db61212dda869
Krishna Murthy <krrish94@gmail.com>
2023-09-23 15:36:48 -0400

    Replace SAM with FastSAM for 1Hz pixel-aligned features
    Signed-off-by: Krishna Murthy <krrish94@gmail.com>

--- examples/extract_conceptfusion_features.py
+++ examples/extract_conceptfusion_features.py
@@ -1,5 +1,7 @@
 import os
 import pickle as pkl
+import sys
+import warnings
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Union
@@ -22,9 +24,18 @@ from gradslam.structures.pointclouds import Pointclouds
 from gradslam.structures.rgbdimages import RGBDImages
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from PIL import Image
-from segment_anything import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
 from tqdm import tqdm, trange
 from typing_extensions import Literal
+
+# Check if an os environment variable is set for fastsam path
+FASTSAM_PYTHON_PATH = None
+if not os.environ.get("FASTSAM_PYTHON_PATH"):
+    warnings.warn(
+        "FASTSAM_PYTHON_PATH not set. Please set it to the path of FastSAM repo."
+    )
+FASTSAM_PYTHON_PATH = "/home/krishna/code/FastSAM"
+sys.path.append(FASTSAM_PYTHON_PATH)
+from fastsam import FastSAM, FastSAMPrompt
 
 
 @dataclass
@@ -33,38 +44,44 @@ class ProgramArgs:
     device: str = "cuda"
 
     # SAM checkpoint and model params
-    checkpoint_path: Union[str, Path] = (
-        Path.home()
-        / "code"
-        / "gradslam-foundation"
-        / "examples"
-        / "checkpoints"
-        / "sam_vit_h_4b8939.pth"
+    sam_model_path: Union[str, Path] = (
+        Path(FASTSAM_PYTHON_PATH) / "weights" / "FastSAM-x.pt"
     )
-    model_type = "vit_h"
-    # Ignore masks that have valid pixels less than this fraction (of the image area)
-    bbox_area_thresh: float = 0.0005
-    # Number of query points (grid size) to be sampled by SAM
-    points_per_side: int = 32
+    # Resize image so that the largest side is this sam_imgsz pixels
+    sam_imgsz: int = 512
+    # IoU threshold (TODO: elaborate)
+    sam_iou: float = 0.9
+    # Mask confidence threshold
+    sam_conf: float = 0.4
+    # Device to run FastSAM on
+    sam_device: str = "cuda:0"
+    # Draw high-res segmentation masks
+    sam_retina: bool = True
+    # Draw contours around masks
+    sam_with_contours: bool = True
+    # Better quality using morphologyEx
+    sam_better_quality: bool = False
+    # Save mask visualizations
+    sam_save_viz: bool = True
 
     # gradslam mode ("incremental" vs "batch")
     mode: Literal["incremental", "batch"] = "incremental"
 
     # Path to the data config (.yaml) file
-    dataconfig_path: str = "dataconfigs/icl.yaml"
+    dataconfig_path: str = "dataconfigs/replica.yaml"
     # Path to the dataset directory
-    data_dir: Union[str, Path] = Path.home() / "data" / "icl"
+    data_dir: Union[str, Path] = Path.home() / "data" / "nice-slam-data" / "Replica"
     # Sequence from the dataset to load
-    sequence: str = "living_room_traj1_frei_png"
+    sequence: str = "room0"
     # Start frame index
     start_idx: int = 0
     # End frame index
     end_idx: int = -1
     # Stride (number of frames to skip between successive fusion steps)
-    stride: int = 20
+    stride: int = 100
     # Desired image width and height
-    desired_height: int = 120
-    desired_width: int = 160
+    desired_height: int = 240
+    desired_width: int = 320
 
     # CLIP model config
     open_clip_model = "ViT-H-14"
@@ -88,12 +105,44 @@ def get_dataset(dataconfig_path, basedir, sequence, **kwargs):
         raise ValueError(f"Unknown dataset name {config_dict['dataset_name']}")
 
 
-def main():
+def crop_bbox_from_img(img, _fastsam_everything_results, idx=0, no_bg=False):
+    bbox_xyxy = _fastsam_everything_results[0].boxes.xyxy[idx]
+    _img = img
+    if no_bg:
+        _mask = _fastsam_everything_results[0].masks.data[idx]
+        _img = _img * _mask[..., None].astype(np.float32)
+        _img = _img.astype(np.uint8)
+    # Check if bbox is torch tensor or numpy array or list
+    if isinstance(bbox_xyxy, torch.Tensor):
+        x1, y1, x2, y2 = [
+            int(_item) for _item in bbox_xyxy.detach().cpu().numpy().tolist()
+        ]
+    elif isinstance(bbox_xyxy, np.ndarray):
+        x1, y1, x2, y2 = [int(_item) for _item in bbox_xyxy.tolist()]
+    elif isinstance(bbox_xyxy, list):
+        x1, y1, x2, y2 = [int(_item) for _item in bbox_xyxy]
+    else:
+        raise TypeError(f"Unknown type for bbox_xyxy: {type(bbox_xyxy)}")
+    return _img[y1:y2, x1:x2]
 
+
+def batch_crop_bbox_from_img(img, _fastsam_everything_results, no_bg=False):
+    num_boxes = _fastsam_everything_results[0].boxes.xyxy.shape[0]
+    cropped_imgs = []
+
+    for idx in range(num_boxes):
+        cropped_img = crop_bbox_from_img(
+            img, _fastsam_everything_results, idx=idx, no_bg=no_bg
+        )
+        cropped_imgs.append(cropped_img)
+    return cropped_imgs
+
+
+def main():
     torch.autograd.set_grad_enabled(False)
 
     args = tyro.cli(ProgramArgs)
-    
+
     # dataconfig = load_dataset_config(args.dataconfig_path)
     dataset = get_dataset(
         dataconfig_path=args.dataconfig_path,
@@ -106,103 +155,102 @@ def main():
         desired_width=args.desired_width,
     )
 
-    sam = sam_model_registry[args.model_type](checkpoint=Path(args.checkpoint_path))
-    sam.to(device=args.device)
-    mask_generator = SamAutomaticMaskGenerator(
-        model=sam,
-        points_per_side=8,
-        pred_iou_thresh=0.92,
-        crop_n_layers=1,
-        crop_n_points_downscale_factor=2,
-    )
-
-    os.makedirs(args.save_dir, exist_ok=True)
-
-    print("Extracting SAM masks...")
-    for idx in trange(len(dataset)):
-        imgfile = dataset.color_paths[idx]
-        img = cv2.imread(imgfile)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        masks = mask_generator.generate(img)
-        cur_mask = masks[0]["segmentation"]
-        _savefile = os.path.join(
-            args.save_dir,
-            os.path.splitext(os.path.basename(dataset.color_paths[idx]))[0] + ".pkl",
-        )
-        with open(_savefile, "wb") as f:
-            pkl.dump(masks, f, protocol=pkl.HIGHEST_PROTOCOL)
+    # Load FastSAM model
+    print("Loading FastSAM model...")
+    fastsam_model = FastSAM(args.sam_model_path)
 
     print(
         f"Initializing OpenCLIP model: {args.open_clip_model}"
         f" pre-trained on {args.open_clip_pretrained_dataset}..."
     )
-    model, _, preprocess = open_clip.create_model_and_transforms(
+    clip_model, _, clip_preprocess = open_clip.create_model_and_transforms(
         args.open_clip_model, args.open_clip_pretrained_dataset
     )
-    model.cuda()
-    model.eval()
+    clip_model.cuda()
+    clip_model.half()
+    clip_model.eval()
 
-    print("Computing pixel-aligned features...")
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    print("Compute pixel-aligned 2D features...")
     for idx in trange(len(dataset)):
-        maskfile = os.path.join(
-            args.save_dir,
-            os.path.splitext(os.path.basename(dataset.color_paths[idx]))[0] + ".pkl",
-        )
-        with open(maskfile, "rb") as f:
-            masks = pkl.load(f)
-        
         imgfile = dataset.color_paths[idx]
         img = cv2.imread(imgfile)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        LOAD_IMG_HEIGHT, LOAD_IMG_WIDTH = img.shape[0], img.shape[1]
-        
-        global_feat = None
-        with torch.cuda.amp.autocast():
-            # print("Extracting global CLIP features...")
-            _img = preprocess(Image.open(imgfile)).unsqueeze(0)
-            global_feat = model.encode_image(_img.cuda())
-            global_feat /= global_feat.norm(dim=-1, keepdim=True)
-            # tqdm.write(f"Image feature dims: {global_feat.shape} \n")
-        global_feat = global_feat.half().cuda()
-        global_feat = torch.nn.functional.normalize(global_feat, dim=-1)  # --> (1, 1024)
+        sam_masks = fastsam_model(
+            imgfile,
+            device=args.sam_device,
+            retina_masks=args.sam_retina,
+            imgsz=args.sam_imgsz,
+            conf=args.sam_conf,
+            iou=args.sam_iou,
+        )
+        cropped_imgs = batch_crop_bbox_from_img(img, sam_masks, no_bg=False)
+
+        # global CLIP feature
+        preprocessed_full_img = clip_preprocess(Image.fromarray(img))
+        global_feat = clip_model.encode_image(
+            preprocessed_full_img[None, ...].half().cuda()
+        )
         feat_dim = global_feat.shape[-1]
-        cosine_similarity = torch.nn.CosineSimilarity(dim=-1)
+        global_feat = torch.nn.functional.normalize(global_feat, dim=-1)
 
-        feat_per_roi = []
-        roi_nonzero_inds = []
+        # Convert to PIL and preprocess
+        preprocessed_images = [
+            clip_preprocess(Image.fromarray(img_))[None, ...].half().to(args.device)
+            for img_ in cropped_imgs
+        ]
+        batch_preprocessed = torch.cat(preprocessed_images, axis=0)
+        local_feat = clip_model.encode_image(batch_preprocessed.half())
+        local_feat = torch.nn.functional.normalize(local_feat, dim=-1)
+
         similarity_scores = []
-        for maskidx in range(len(masks)):
-            _x, _y, _w, _h = tuple(masks[maskidx]["bbox"])  # xywh bounding box
-            seg = masks[maskidx]["segmentation"]
-            nonzero_inds = torch.argwhere(torch.from_numpy(masks[maskidx]["segmentation"]))
-            # Note: Image is (H, W, 3). In SAM output, y coords are along height, x along width
-            img_roi = img[_y : _y + _h, _x : _x + _w, :]
-            img_roi = Image.fromarray(img_roi)
-            img_roi = preprocess(img_roi).unsqueeze(0).cuda()
-            roifeat = model.encode_image(img_roi)
-            roifeat = torch.nn.functional.normalize(roifeat, dim=-1)
-            feat_per_roi.append(roifeat)
-            roi_nonzero_inds.append(nonzero_inds)
-            _sim = cosine_similarity(global_feat, roifeat)
-            similarity_scores.append(_sim)
-        
-        similarity_scores = torch.cat(similarity_scores)
-        softmax_scores = torch.nn.functional.softmax(similarity_scores, dim=0)
-        outfeat = torch.zeros(LOAD_IMG_HEIGHT, LOAD_IMG_WIDTH, feat_dim, dtype=torch.half)
-        for maskidx in range(len(masks)):
-            _weighted_feat = softmax_scores[maskidx] * global_feat + (1 - softmax_scores[maskidx]) * feat_per_roi[maskidx]
-            _weighted_feat = torch.nn.functional.normalize(_weighted_feat, dim=-1)
-            outfeat[roi_nonzero_inds[maskidx][:, 0], roi_nonzero_inds[maskidx][:, 1]] += _weighted_feat[0].detach().cpu().half()
-            outfeat[roi_nonzero_inds[maskidx][:, 0], roi_nonzero_inds[maskidx][:, 1]] = torch.nn.functional.normalize(
-                outfeat[roi_nonzero_inds[maskidx][:, 0], roi_nonzero_inds[maskidx][:, 1]].float(), dim=-1
-            ).half()
+        semiglobal_feat = []
+        cosine_similarity = torch.nn.CosineSimilarity(dim=-1)
+        for local_feat_idx in range(local_feat.shape[0]):
+            similarity_scores.append(
+                cosine_similarity(global_feat, local_feat[local_feat_idx])
+            )
+        softmax_scores = torch.nn.functional.softmax(
+            torch.cat(similarity_scores), dim=0
+        )
+        for local_feat_idx in range(local_feat.shape[0]):
+            w = softmax_scores[local_feat_idx][None, ..., None]
+            wfeat = w * global_feat + (1 - w) * local_feat[local_feat_idx]
+            semiglobal_feat.append(torch.nn.functional.normalize(wfeat, dim=-1))
 
-        outfeat = outfeat.unsqueeze(0).float()  # interpolate is not implemented for float yet in pytorch
-        outfeat = outfeat.permute(0, 3, 1, 2)  # 1, H, W, feat_dim -> 1, feat_dim, H, W
-        outfeat = torch.nn.functional.interpolate(outfeat, [args.desired_height, args.desired_width], mode="nearest")
-        outfeat = outfeat.permute(0, 2, 3, 1)  # 1, feat_dim, H, W --> 1, H, W, feat_dim
-        outfeat = torch.nn.functional.normalize(outfeat, dim=-1)
-        outfeat = outfeat[0].half() # --> H, W, feat_dim
+        prompt_process = FastSAMPrompt(imgfile, sam_masks, device=args.sam_device)
+        masks = prompt_process.everything_prompt()
+        # masks is N, H, W; convert to N, 1, H, W
+        masks = masks[:, None, ...]
+        # Downsample masks to target shape (nearest neighbor interpolation)
+        masks = torch.nn.functional.interpolate(
+            masks.float(), [args.desired_height, args.desired_width], mode="nearest"
+        ).bool()
+        masks = masks.squeeze(-3)
+
+        mask_areas = []
+        for maskidx in range(len(masks)):
+            xywh = sam_masks[0].boxes[maskidx].xywh[0]
+            mask_areas.append(xywh[2].item() * xywh[3].item())
+
+        # Sort the mask indices in decreasing order of area
+        sorted_mask_indices = np.argsort(np.array(mask_areas))[::-1]
+
+        outfeat = torch.zeros(
+            args.desired_height,
+            args.desired_width,
+            feat_dim,
+            dtype=torch.half,
+            device=args.device,
+        )
+
+        for maskidx in sorted_mask_indices.tolist():
+            nonzero_inds = torch.argwhere(masks[maskidx])
+            # Store the semiglobal feature for this mask
+            outfeat[nonzero_inds[:, 0], nonzero_inds[:, 1]] = semiglobal_feat[
+                maskidx
+            ].half()
 
         # tokenizer = open_clip.get_tokenizer(args.open_clip_model)
         # while True:
@@ -212,7 +260,7 @@ def main():
         #         break
 
         #     text = tokenizer([prompt_text])
-        #     textfeat = model.encode_text(text.cuda())
+        #     textfeat = clip_model.encode_text(text.cuda())
         #     textfeat = torch.nn.functional.normalize(textfeat, dim=-1)
         #     textfeat = textfeat.unsqueeze(0)
 
@@ -236,7 +284,6 @@ def main():
             os.path.splitext(os.path.basename(dataset.color_paths[idx]))[0] + ".pt",
         )
         torch.save(outfeat.detach().cpu(), savefile)
-        
 
 
 if __name__ == "__main__":
